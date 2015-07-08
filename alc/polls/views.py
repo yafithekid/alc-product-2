977466b70e4678533e03badcf086cd62465fe138
Muhammad Yafi <yafithekid212@gmail.com>
2015-07-08 07:09:30 +0700

    coba-coba

---
+++ alc/polls/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+def index(request):
+  template = loader.get_template('polls/index.html')
+  context = RequestContext(request,{'name':'koyek'})
+  return HttpResponse(template.render(context))
+
+def detail(request, question_id):
+  return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+  response = "You're looking at the results of question %s."
+  return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+  return HttpResponse("You're voting on question %s." % question_id)
--- alc/urls.py
+++ alc/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from alc.polls import urls as polls_urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^polls/', include(polls_urls))
 ]
