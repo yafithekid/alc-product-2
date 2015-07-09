50150307bbfe2eab5654dda51f69d8bdfa68f2b1
Muhammad Yafi <t-yafi@traveloka.com>
2015-07-09 16:43:22 +0700

    resolve conflict

---
+++ lc/tryout/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from lc.models import *
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+# Create your views here.
+def index(request):
+	template = loader.get_template("tryout/index.html")
+	context = RequestContext(request,{})
+ 	return HttpResponse(template.render(context))
--- lc/urls.py
+++ lc/urls.py
@@ -1,8 +1,12 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 from . import views
+from lc.problem import urls as problem_urls
+from lc.tryout import urls as tryout_urls
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index')
+    #url(r'^$', views.index, name='index')
+    url(r'^problem/$',include(problem_urls)),
+    url(r'^tryout/$',include(tryout_urls))
 ]
