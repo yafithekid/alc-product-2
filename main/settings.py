54b3f7e141e56da52d3b357bbaaf5897cc36b378
Muhammad Yafi <yafithekid212@gmail.com>
2015-08-11 21:33:54 +0700

    remove MYSQL config because we use mongodb

--- main/settings.py
+++ main/settings.py
@@ -77,14 +77,6 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'alc',
-        'USER' : 'alc',
-        'PASSWORD' : 'alc',
-        'HOST': '127.0.0.1',
-        'PORT': 3306
-    }
 }
 
 
